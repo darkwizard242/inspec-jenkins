b4d26a147e03422c6b858a3d33597739d1bd92f5
darkwizard242 <am900820@gmail.com>
2020-01-08 20:40:16 -0500

    check for jenkins home directory

--- controls/01_jenkins.rb
+++ controls/01_jenkins.rb
@@ -15,18 +15,6 @@ end
 
 control "jenkins-02" do
   impact 0.7
-  title "Validate jenkins repo file"
-  desc "Control to validate whether jenkins repo file exists or not."
-
-  describe apt('http://pkg.jenkins.io/debian-stable') do
-    it { should exist }
-    it { should be_enabled }
-  end
-end
-
-
-control "jenkins-03" do
-  impact 0.7
   title "Validate jenkins service is present, enabled and running."
   desc "Control to validate whether a service file exists for jenkins, it is in enabled state and is in running state."
 
@@ -38,12 +26,25 @@ control "jenkins-03" do
 end
 
 
-control "jenkins-04" do
+control "jenkins-03" do
   impact 0.7
-  title "Verify jenkins process is running via jenkins user."
-  desc "Control to check whether there is a 'jenkins' process is running and is owned by user 'jenkins'."
+  title "Verify jenkins process is running via jenkins user and group."
+  desc "Control to check whether there is a 'jenkins' process is running and is owned by user/group 'jenkins'."
 
   describe processes('java') do
     its('users') { should eq ['jenkins', 'jenkins'] }
+  end
+end
+
+
+control "jenkins-04" do
+  impact 0.7
+  title "Verify jenkins home directory exists."
+  desc "Control to check whether there the default home directory for jenkins exists."
+
+  describe directoy('/var/lib/jenkins') do
+    its('owner') { should eq 'jenkins' }
+    its('group') { should eq 'jenkins' }
+    its('mode') { should cmp '0755' }
   end
 end
